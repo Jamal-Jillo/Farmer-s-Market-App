eb7210a0222b07c7673558dc6b7bc4d4db397765
Jamal <jayjamal86@gmail.com>
2023-02-10 19:33:17 +0300

    included __init__ file

---
+++ mkt_app/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '5367eac0ee1931204394865b0ad6be06'
+# This is a random string of characters that is used to encrypt the form data
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+# The database URI is the location of the database file
+db = SQLAlchemy(app)
+
+app.app_context().push()  # Flask-SQLAlchemy requires an application context
+
+from mkt_app import routes
