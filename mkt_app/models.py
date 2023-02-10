4d7acd5ffd789dfd3ee7b369dc5cc667962c7617
Jamal <jayjamal86@gmail.com>
2023-02-10 19:35:48 +0300

    database models using flask-sqlalchemy

---
+++ mkt_app/models.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+from datetime import datetime
+from mkt_app import db
+from flask_sqlalchemy import SQLAlchemy
+
+
+class User(db.Model):
+    """User model.
+
+    Arguments:
+        db -- Database object
+
+    Returns:
+        User object
+    """
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    def __repr__(self):
+        """Return a string representation of the User object."""
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+
+class Post(db.Model):
+    """Post model."""
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False,
+                            default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        """Return a string representation of the Post object."""
+        return f"Post('{self.title}', '{self.date_posted}')"
