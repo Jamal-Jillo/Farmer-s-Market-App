5e822bc80d1808996f0847e90d83c81bbc10454b
Jamal <jayjamal86@gmail.com>
2023-02-10 19:34:28 +0300

    registration and login forms for the app

---
+++ mkt_app/forms.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""Forms for the Farmers-Market application.(Registration and Login)."""
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, length, Email, EqualTo
+
+
+class RegistrationForm(FlaskForm):
+    """Registration form."""
+
+    username = StringField('Username',
+                           validators=[DataRequired(), length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm_Password',
+                                     validators=[DataRequired(),
+                                                 EqualTo('password')])
+    SubmitField = SubmitField('Sign Up')
+
+
+class LoginForm(FlaskForm):
+    """Login form."""
+
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    SubmitField = SubmitField('Login')
